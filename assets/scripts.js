026f1b0073276d63db6d3ce246f98f3908a77268
adesanyaolu <sanya4reel@gmail.com>
2023-12-23 22:10:48 +0100

    Playing around

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,2 +1,11 @@
-const wholeBody = document.getElementsByClassName('wholeBody');
-wholeBody.style.borderBottom = "2px solid red";
+const subscribeButton = document.querySelector(".button");
+const subscribePage = document.querySelector(".wholeBody");
+const successPage = document.querySelector(".successState");
+
+console.log(subscribeButton, subscribePage, successPage)
+
+subscribeButton.addEventListener("click", function() {
+    // This function will be executed when the button is clicked
+    subscribePage.style.display = subscribePage.style.display === 'none' ? 'block' : 'none';
+    successPage.style.display = successPage.style.display === 'none' ? 'block' : 'block';
+});
