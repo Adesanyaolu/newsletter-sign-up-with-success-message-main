97f1d1b391a85369536fc562dd721fa49c37bca7
adesanyaolu <sanya4reel@gmail.com>
2024-01-03 17:52:53 +0100

    Javascript completed and adjusting the styling

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,11 +1,29 @@
-const subscribeButton = document.querySelector(".button");
-const subscribePage = document.querySelector(".wholeBody");
-const successPage = document.querySelector(".successState");
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".wholeBody");
+const card_2 = document.querySelector(".successState");
+const inputBox = document.querySelector("#email");
+const sucessButton = document.querySelector(".sucessButton");
 
-console.log(subscribeButton, subscribePage, successPage)
+// Displays the success page
 
-subscribeButton.addEventListener("click", function() {
-    // This function will be executed when the button is clicked
-    subscribePage.style.display = subscribePage.style.display === 'none' ? 'block' : 'none';
-    successPage.style.display = successPage.style.display === 'none' ? 'block' : 'block';
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  inputBox.value.trim() === ""
+    ? alert("Email field is empty")
+    : (card_1.classList.add("hide"), card_2.classList.remove("hide"));
+
+  // if (inputBox.value.trim() === "") {
+  //     alert("Email field is empty");
+  // } else {
+  //     card_1.classList.add('hide');
+  //     card_2.classList.remove('hide');
+  // }
+});
+
+// Returns to the form page
+sucessButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  card_1.classList.remove("hide");
+  card_2.classList.add("hide");
 });
