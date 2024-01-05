534da624b86ecd5333d019bffa6580646fbc9520
adesanyaolu <sanya4reel@gmail.com>
2024-01-05 09:40:25 +0100

    Updated the email that appears on the success page

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,6 +3,7 @@ const card_1 = document.querySelector(".wholeBody");
 const card_2 = document.querySelector(".successState");
 const inputBox = document.querySelector("#email");
 const sucessButton = document.querySelector(".sucessButton");
+const userEmail = document.getElementById("userEmail");
 
 // Displays the success page
 
@@ -12,13 +13,7 @@ form.addEventListener("submit", function (e) {
   inputBox.value.trim() === ""
     ? alert("Email field is empty")
     : (card_1.classList.add("hide"), card_2.classList.remove("hide"));
-
-  // if (inputBox.value.trim() === "") {
-  //     alert("Email field is empty");
-  // } else {
-  //     card_1.classList.add('hide');
-  //     card_2.classList.remove('hide');
-  // }
+    userEmail.innerHTML = (inputBox.value)
 });
 
 // Returns to the form page
